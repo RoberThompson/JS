6220cfa7f0edb967fe27f61928de41c71913e33c
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-08-25 15:35:21 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,3 +3,18 @@
 function randomRange(myMin, myMax) {
   return Math.floor(Math.random()*((myMax)-(myMin-1)))+myMin;
 }
+
+//puts code in an array in decending order.
+// Only change code below this line
+function countdown(n){
+  if(n<1){
+    return [];}
+  else{
+    const countArr=countdown(n-1);
+    countArr.unshift(n);
+    return countArr;
+  }
+    
+}
+// Only change code above this line
+console.log(countdown(5));
