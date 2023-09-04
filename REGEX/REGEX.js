f6be06770fbcec692fbac9f88ba02957b856f0c7
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-09-04 13:23:52 -0400

    Update REGEX.js

--- REGEX/REGEX.js
+++ REGEX/REGEX.js
@@ -7,3 +7,23 @@ let result = myRegex.test(myString);
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
 let result = petRegex.test(petString);
+
+//ignore case(letter Case ig upper/lower) using i flag.
+// example /ignorecase/i would match ignorecase , 
+// igNoreCase , IgnoreCase.
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+// extract actual matches in the string with match()
+//
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+
+//to seach a pattern more than once
+//use global search flag g
+//this also uses i to disregard letter case.
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
