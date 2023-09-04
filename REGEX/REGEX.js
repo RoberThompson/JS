793e1883e39e64a25ab5a2660505bce050918d0f
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-09-04 13:07:18 -0400

    Update REGEX.js

--- REGEX/REGEX.js
+++ REGEX/REGEX.js
@@ -2,3 +2,8 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); 
+
+//checking for multiple words using | operator.
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
