23247f4aa17c0c7281d43d3a83faeaa34375ba2e
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-09-11 21:53:21 -0400

    Update REGEX.js

--- REGEX/REGEX.js
+++ REGEX/REGEX.js
@@ -51,3 +51,9 @@ let quoteSample = "Blueberry 3.141592653s are delicious.";
 let myRegex = /[h-s2-6]/ig; // Change this line
 let result =quoteSample.match(myRegex); // Change this line
 
+//not including vowels and numbers using ^
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou0-9]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+console.log(result);
+
