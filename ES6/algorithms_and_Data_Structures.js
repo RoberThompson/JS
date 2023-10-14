e411539d84d2977cb3e131419dd308db4fff451a
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-10-13 22:12:52 -0400

    Update algorithms_and_Data_Structures.js

--- ES6/algorithms_and_Data_Structures.js
+++ ES6/algorithms_and_Data_Structures.js
@@ -66,3 +66,39 @@ delete foods.strawberries;
 // Only change code above this line
 
 console.log(foods);
+
+//check if objex has a property
+//in either of the following ways:
+
+//users.hasOwnProperty('Alan');
+//'Alan' in users;
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(userObj) {
+  // Only change code below this line
+  if('Alan' in userObj && 'Jeff' in userObj && 'Sarah' in userObj && 'Ryan' in userObj){
+return true;
+  }
+  else{return false;}
+  // Only change code above this line
+}
+
+console.log(isEveryoneHere(users));
