5eff0855533b87dcfcdec2688cd8d73a23eab0df
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-10-13 22:07:25 -0400

    Update algorithms_and_Data_Structures.js

--- ES6/algorithms_and_Data_Structures.js
+++ ES6/algorithms_and_Data_Structures.js
@@ -30,3 +30,39 @@ userActivity.data.online=45;
 // Only change code above this line
 
 console.log(userActivity);
+
+//objex
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  // Only change code below this line
+  return foods[scannedItem]; 
+  // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
+
+//objex... delete key values.
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+// Only change code below this line
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+// Only change code above this line
+
+console.log(foods);
