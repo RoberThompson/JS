a70e12f5438943b33ef8147febca1280f8fb73df
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-10-14 13:26:27 -0400

    Update algoritms.js

--- algoritms.js
+++ algoritms.js
@@ -25,4 +25,13 @@ function largestOfFour(arr) {
 console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]));
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 //
-//
+//confirm ending
+function confirmEnding(str, target) {
+ 
+ if(str.slice(str.length-target.length,str.length)===target){return true;}
+ else{return false;}
+   
+}
+
+confirmEnding("Bastian", "n");
+console.log(confirmEnding("He has to give me a new name", "name"));
