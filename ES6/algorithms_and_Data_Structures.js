25720af2d19b3cd905bc62c5092d4d9198fa1680
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-10-13 21:58:24 -0400

    Update algorithms_and_Data_Structures.js

--- ES6/algorithms_and_Data_Structures.js
+++ ES6/algorithms_and_Data_Structures.js
@@ -14,3 +14,19 @@ function filteredArray(arr, elem) {
 }
 
 console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+
+//objex
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+
+// Only change code below this line
+userActivity.data.online=45;
+// Only change code above this line
+
+console.log(userActivity);
