bb0ff7f2cd1404d4363c2e7001f3688be2555a1f
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-10-15 23:06:27 -0400

    Update algoritms.js

--- algoritms.js
+++ algoritms.js
@@ -135,3 +135,30 @@ getIndexToIns([40, 60], 50);
 getIndexToIns([10, 20, 30, 40, 50], 35);
 getIndexToIns([5, 3, 20, 3], 5);
  getIndexToIns([2, 20, 10], 19);
+
+
+/*Mutations
+
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+*/
+function mutation(arr) {
+
+  const test = arr[1].toLowerCase();
+  const target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+
+
+mutation(["hello", "hey"]);
+mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]);
+console.log(mutation(["Mary", "Army"]));
+console.log(mutation(["hello", "hey"]));
