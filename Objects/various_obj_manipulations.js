0450c013dfe46cb7b7c7e43ef2a867b5f0204869
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-10-16 09:12:19 -0400

    Update various_obj_manipulations.js

--- Objects/various_obj_manipulations.js
+++ Objects/various_obj_manipulations.js
@@ -8,3 +8,16 @@ let dog = {
 };
 
 dog.sayLegs();
+
+//using this keyword
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  //pitfall here. If varaible name cahnges, any code referencing original name
+  //would need to be updated.
+  //A way to avoid this would be to use 'this' keyword.
+  //Makes code reuseable.
+  sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+};
+
+dog.sayLegs();
