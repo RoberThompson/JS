5e3fdef2457081303f8f8c5b1e3564c212e46646
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-10-15 23:25:18 -0400

    Create various_obj_manipulations.js

---
+++ Objects/various_obj_manipulations.js
@@ -0,0 +1,10 @@
+//adding a method sayLegs
+//to the objex
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function(){return "This dog has " + dog.numLegs +" legs.";}
+
+};
+
+dog.sayLegs();
