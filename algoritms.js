125af55cb92a20890d19e1d496dcc0c98dfdd879
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-10-15 22:30:34 -0400

    Update algoritms.js

--- algoritms.js
+++ algoritms.js
@@ -109,3 +109,29 @@ function bouncer(arr) {
 }
 
 bouncer([7, "ate", "", false, 9]);
+
+/*
+
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+ */
+
+function getIndexToIns(arr, num) {
+  let index=0;
+  //taken from w3 schools for using sort with numbers.
+  arr.sort(function(a, b){return a - b});
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]<num ){index+=1;}
+  }
+  console.log(arr);
+  console.log(index);
+  return index;
+}
+
+getIndexToIns([40, 60], 50);
+getIndexToIns([10, 20, 30, 40, 50], 35);
+getIndexToIns([5, 3, 20, 3], 5);
+ getIndexToIns([2, 20, 10], 19);
