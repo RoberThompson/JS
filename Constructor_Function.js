84133b24e499dd50fe9c0f66344b1a5024b39dd4
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-10-16 10:17:13 -0400

    Update Constructor_Function.js

--- Constructor_Function.js
+++ Constructor_Function.js
@@ -91,3 +91,43 @@ function Dog(name) {
 // Only change code above this line
 let beagle = new Dog("Snoopy");
 Dog.prototype.numLegs = 4;
+
+
+/*Iterate Over All Properties
+
+You have now seen two kinds of properties: 
+own properties and prototype properties. 
+Own properties are defined directly on the object instance itself. 
+And prototype properties are defined on the prototype.
+
+function Bird(name) {
+  this.name = name;  //own property
+}
+
+Bird.prototype.numLegs = 2; // prototype property
+
+let duck = new Bird("Donald");
+
+
+Add all of the own properties of beagle to the array ownProps. 
+Add all of the prototype properties of Dog to the array prototypeProps.
+*/
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Only change code below this line
+for(let property in beagle){
+  if(beagle.hasOwnProperty(property)){
+    ownProps.push(property);
+  }
+  else{prototypeProps.push(property);}
+}
