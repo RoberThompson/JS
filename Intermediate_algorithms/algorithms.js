0fd778339d2dcd65598cee2d4bcff3a2650678b6
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-12-03 10:23:04 -0500

    Update algorithms.js

--- Intermediate_algorithms/algorithms.js
+++ Intermediate_algorithms/algorithms.js
@@ -56,3 +56,43 @@ destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 
 //***************
 //***************
+/*Make a function that looks through an array of objects (first argument) 
+and returns an array of all objects that have matching name and value pairs (second argument). 
+Each name and value pair of the source object has to be present in the object from the 
+collection if it is to be included in the returned array.*/
+
+function whatIsInAName(collection, source) {
+
+  const sourceKeys = Object.keys(source);
+
+  return collection
+    .filter(obj => sourceKeys
+      .every(key => obj[key] === source[key]));
+}
+
+//or we could do
+function whatIsInAName(collection, source) {
+  const souceKeys = Object.keys(source);
+
+  // filter the collection
+  return collection.filter(obj => {
+    for (let i = 0; i < sourceKeys.length; i++) {
+      if (obj[sourceKeys[i]] !== source[sourceKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+//for testing output
+console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+
+//***************
+//***************
+
+
+
+
+
+
+
